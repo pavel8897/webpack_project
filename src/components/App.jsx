9f4f29ed842a1b140ea0bf5ab10eaaff639f00e2
Pave Nikiforov <pavel8897@bk.ru>
2022-11-18 16:44:18 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.less'
+
+const App = () => {
+    return (
+        <div className='app'>
+            App!!!
+        </div>
+    )
+}
+
+export default App
